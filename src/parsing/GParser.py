58bff78ad0d4dde2a05a890e3d256b83b0c2587f
gordonbuck <grdnbuck@googlemail.com>
2018-02-08 12:38:02 +0000

    added xml parsing option

--- src/parsing/GParser.py
+++ src/parsing/GParser.py
@@ -7,6 +7,33 @@ from urllib import request
 
 url = "http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1516338822280.pdf"
 
+def convert_pdf(url, format='text', codec='utf-8', password=''):
+    rsrcmgr = PDFResourceManager()
+    retstr = BytesIO()
+    laparams = LAParams()
+    if format == 'text':
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    elif format == 'html':
+        device = HTMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    elif format == 'xml':
+        device = XMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    else:
+        raise ValueError('provide format, either text, html or xml!')
+    remoteFile = request.urlopen(url).read()
+    fp = BytesIO(remoteFile)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue().decode()
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
 def convert_pdf_to_txt(url):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
