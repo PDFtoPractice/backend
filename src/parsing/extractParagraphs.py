957074da7aed7f474dd2e0327a72e8f0c5023ecb
hey-now <trebaczmaja@gmail.com>
2018-02-11 00:20:19 +0000

    Fixes of some special cases

--- src/parsing/extractParagraphs.py
+++ src/parsing/extractParagraphs.py
@@ -12,8 +12,8 @@ def extract_paragraphs(xml_string):
     paragraphs = []
 
     # Compute average text size
-    size_num = 0;
-    text_num = 0;
+    size_num = 0
+    text_num = 0
     for text in root.iter("text"):
         size = text.get("size")
         if (size):
@@ -32,16 +32,16 @@ def extract_paragraphs(xml_string):
         width_sum += width
     average_line_width = width_sum / line_num
 
-
     current_paragraph = ""
     start_line = 0
     line_num = 0
     previous_line_font_size = 0
+    previous_line_text = ''
 
     for line in root.iter("textline"):
         line_text_size_sum = 0
         line_length = 0
-        line_text= ""
+        line_text = ""
 
         bbox = line.get('bbox')
         params = bbox.split(',')
@@ -49,7 +49,7 @@ def extract_paragraphs(xml_string):
 
         for text in line.iter("text"):
             size = text.get("size")
-            if (size):
+            if size:
                 line_text_size_sum += float(size)
                 line_length += 1
                 line_text += text.text
@@ -57,9 +57,10 @@ def extract_paragraphs(xml_string):
         line_font_size = line_text_size_sum/line_length if line_length > 0 else 0
 
         # Make a pragraph division
-        if line_font_size > (previous_line_font_size + 2):
-            print(current_paragraph)
-            paragraphs.append(current_paragraph)
+        if line_font_size > (previous_line_font_size + 2) or re.match(r'^\s*$', previous_line_text):
+            if not re.match(r'^\s*$', current_paragraph):
+                print("--------------\n" + current_paragraph)
+                paragraphs.append(current_paragraph)
             current_paragraph = ""
 
         # Delete whitespace at the beginning of the line
@@ -71,20 +72,24 @@ def extract_paragraphs(xml_string):
                 and not current_paragraph.endswith('\n'):
             line_text = "\n" + line_text
 
-        if line_width < (average_line_width - 20):
+        if line_width < (average_line_width - 20) or re.match(r'^.*:\s*$', line_text):
             line_text += "\n"
 
         if not current_paragraph.endswith(' ') and not current_paragraph.endswith('\n') and not current_paragraph == '':
             current_paragraph += " "
         current_paragraph += line_text
+        previous_line_text = line_text
         previous_line_font_size = line_font_size
         line_num += 1
 
+    if not re.match(r'^\s*$', current_paragraph):
+        print("--------------\n" + current_paragraph)
+        paragraphs.append(current_paragraph)
     print(paragraphs)
     return paragraphs
 
 
-str = GParser.convert_pdf(url, format='xml')
+str = GParser.convert_pdf(url2, format='xml')
 file = open('sample_outputs/orginalXML.xml', 'w', encoding="utf8")
 file.write(str)
 extract_paragraphs(str)
