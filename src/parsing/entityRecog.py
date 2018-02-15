c0b80a4dec111b766d9c68c4c0648fd88a4bebc7
Amrita Panesar <09panesara@gmail.com>
2018-02-15 09:16:26 +0000

    [upd] successfully extracts active substances from some pdfs

--- src/parsing/entityRecog.py
+++ src/parsing/entityRecog.py
@@ -2,15 +2,11 @@ import GParser
 import ClearXML
 import extractParagraphs as ExtractParas
 import re
-
+import spacy
+nlp = spacy.load('en')
 urls = ['http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1492496435313.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1510292397494.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1512713289515.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1517548373003.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1515735504413.pdf']
-
-docs = []
-for url in urls:
-    str = GParser.convert_pdf(url, format='xml')
-    paras = ExtractParas.extract_paragraphs(str)
-    str = ClearXML.clear_XML_from_text_tags(str)
-    docs.append(paras)
+docs = [GParser.convert_pdf(url, format='xml') for url in urls]
+docs = [ExtractParas.extract_paragraphs(parsed) for parsed in docs]
 
 # extract headings paras
 headings = ['1. What (\s)+ is and what is is used for', '2. What you need to know before you are given ProHance', '3. How you are given (\s)+', '4. Possible side effects', '5. How to store [\s]+', '6. Further Information']
@@ -19,26 +15,36 @@ headings = ['1. What (\s)+ is and what is is used for', '2. What you need to kno
 def get_active_subst(paras): # pass in paras for a drug's leaflet
     # print(docs[0])
     for para in paras:
-        start_index = -1
-        search = re.search(headings[5].lower(), para.lower())
-        if(search != None):
-            start = search.start()
-            relev_txt = para[start:]
-            print("RELEVANT ACTIVE SUBSTANCE TEXT")
+        phrases = ['active substance is[\w\s]+.', 'active substances are [\w\s]+.', 'contains [\w]+ as the active ingredient', 'contains[\w]+ as the active ingredients']
+        match = [('active substance is', ''), ('active substances are', ''), ('contains', 'as the active ingredient'), ('contains', 'as the active ingredients')]
+        for i in range(len(phrases)):
+            act_srch = re.search(phrases[i], para)
+            if act_srch != None:
+                sentence = para[act_srch.start():act_srch.end()]
+                spcy_sentence = nlp(u'' + sentence)
+                for token in spcy_sentence:
+                    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
+                begin_index = re.search(match[i][0], sentence).end()
+                end_index = re.search(match[i][1], sentence).start()
+                if(match[i][1] == ''):
+                    active_subst = sentence[begin_index+1:]
+                else:
+                    active_subst = sentence[begin_index+1:end_index]
+                if active_subst[-1] == " ":
+                    active_subst = active_subst[:-1]
+                phrase = active_subst
+                active_subst = ""
+                for word in phrase.split(" "):
+                    if word[-1] in [',', ":", '.']:
+                        word = word[:-1]
+                    nlp_word = nlp.vocab.strings[word]
+                    if word not in nlp.vocab: # ugly hack - probably a better rule we can use
+                        active_subst += word + " "
+                return active_subst.strip()
 
-            phrases = ['active substance is [\s]+.', 'active substances are [\s].', 'contains [\s]+ as the active ingredient', 'contains [\s]+ as the active ingredients']
-            for i in range(len(phrases)):
-                act_match = re.match(phrases[i], relev_txt)
-                if (act_match != None):
-                    start = act_match.start()
-                    end = act_match.end()
-                    print(relev_txt[start:end])
-                    break
-                # closest_txt = relev_txt[actsub_index.end():]
 
-            break
+# TODO: The other ingredients are: in 4th
+active_substances = [get_active_subst(doc) for doc in docs]
+print(active_substances) # ['pancuronium bromide', None, None, 'gadoteridol', None]
 
-    # no match found in 6th section
-    # search through rest of text for active substance, find closest mention of active substance
-
-get_active_subst(docs[0])
+# https://spacy.io/usage/linguistic-features#section-rule-based-matching
