cf336a5f13d61222e28531ba9c74c87e0f65e88b
Amrita Panesar <09panesara@gmail.com>
2018-02-28 10:47:04 +0000

    [upd] updated aliases script

--- src/parsing/entityRecog.py
+++ src/parsing/entityRecog.py
@@ -14,8 +14,8 @@ headings = ['1. What (\s)+ is and what it is used for', '2. What you need to kno
 # receives text of drug leaflet, returns list of active substances if any can be found otherwise returns empty list
 def get_active_subst(text, nlp):
     only_one = True    # whether there are more than one active substance to extract
-    phrases = ['active substance is[\w\s]+.', 'active substances are([\w\s][,]*)+.', 'contains[\w\s]+ as the active ingredient', 'contains[\w]+ as the active ingredients', 'active ingredient is[\w\s]+.', 'contains[\w\s|.]+ of the active substance', 'The active ingredient,[\w\s]+, is', '[\w\s]+ \(active ingredient\)']
-    match = [('active substance is', '', only_one), ('active substances are', '', not only_one), ('contains', 'as the active ingredient', only_one), ('contains', 'as the active ingredients', not only_one), ('active ingredient is', '', only_one), ('contains', 'of the active substance', only_one), ('The active ingredient,', ', is', only_one), ('', '\(active ingredient\)', only_one)]
+    phrases = ['active substance is[\w\s]+.', 'active substances are([\w\s][,]*)+.', 'contains[\w\s]+ as the active ingredient', 'contains[\w]+ as the active ingredients', 'active ingredient is[\w\s]+.', 'contains[\w\s|.]+ of the active substance', 'The active ingredient,[\w\s]+, is', '[\w\s]+ \(active ingredient\)', 'contains the active substance[\w\s]+', 'contains the active substances[\w\s]+', 'contains the active ingredient[\w\s]+', 'contains the active ingredients[\w\s]+']
+    match = [('active substance is', '', only_one), ('active substances are', '', not only_one), ('contains', 'as the active ingredient', only_one), ('contains', 'as the active ingredients', not only_one), ('active ingredient is', '', only_one), ('contains', 'of the active substance', only_one), ('The active ingredient,', ', is', only_one), ('', '\(active ingredient\)', only_one), ('contains the active substance', '', only_one), ('contains the active substances', '', not only_one),  ('contains the active ingredient', '', only_one), ('contains the active ingredients', '', not only_one)]
     for i in range(len(phrases)):
         act_srch = re.search(phrases[i], text)
         actv_substances = []
@@ -42,7 +42,6 @@ def get_active_subst(text, nlp):
                     is_end = True
                 if word not in nlp.vocab:
                     curr_actv_subst += word + " "
-                    print(curr_actv_subst)
                     if word == words[len(words)-1]: # if last word is active substance word, append to curr_actv_subst
                         curr_actv_subst = curr_actv_subst.strip()
                         actv_substances.append(curr_actv_subst)
@@ -62,11 +61,10 @@ def get_active_subst(text, nlp):
             return actv_substances
     return []
 
-
+# deal with Topical corticosteroids are able to reduce the inflammation caused by a variety of skin condition
 def get_med_for(para, nlp): # extract purpose e.g. 'stop blood clot' -> 'medicine to stop blood clot' 'medicine to help to stop blood clots'
-    from spacy.lemmatizer import Lemmatizer
     purpose = []
-    phrases = ['help to[\w\s]+.', 'helps to[\w\s]+.', 'used to[\w\s]+.', 'used for[\w\s]+.']
+    phrases = ['help to[\w\s]+.', 'helps to[\w\s]+.', 'used to[\w\s]+.', 'used for[\w\s]+.', 'are able to[\w\s]+', 'is able to[\w\s]+']
     for phrase in phrases:
         srch = re.search(phrase, para)
         if srch != None:
@@ -74,6 +72,7 @@ def get_med_for(para, nlp): # extract purpose e.g. 'stop blood clot' -> 'medicin
             match = srch.group(0)
             seenFirstVerb = False
             doc = nlp(u'' + match)
+            span = None
             for i in range(len(doc)):
                 token = doc[i]
                 if token.pos_ == "VERB":
@@ -87,56 +86,87 @@ def get_med_for(para, nlp): # extract purpose e.g. 'stop blood clot' -> 'medicin
             index = -1 # index of ADP
             purpose1 = ''
             lemmas = []
-            for i in range(len(span)):
-                token = span[i]
-                if token.pos_ == "NOUN":
-                    if i!=(len(span)-1) and span[i+1].pos_ != "NOUN":
-                        purpose1 = span[:i+1]
-                        for lemma in lemmas:
-                            purpose1txt = purpose1.text
-                            purpose1 = purpose1txt.replace(lemma[0], lemma[1])
-                        purpose.append(purpose1)
-                        if (len(span)-1) != i and span[i+1].pos_ == "ADP":
-                            index = i+1
-                        break
-                elif token.pos_ == "VERB":
-                    lemma = (token.text, token.lemma_)
-                    lemmas.append(lemma)
+            if span != None:
+                for i in range(len(span)):
+                    token = span[i]
+                    if token.pos_ == "NOUN":
+                        if i!=(len(span)-1) and span[i+1].pos_ != "NOUN":
+                            purpose1 = span[:i+1]
+                            for lemma in lemmas:
+                                if not isinstance(purpose1, str):
+                                    purpose1txt = purpose1.text
+                                purpose1 = purpose1txt.replace(lemma[0], lemma[1])
+                            purpose.append(purpose1)
+                            if (len(span)-1) != i and span[i+1].pos_ == "ADP":
+                                index = i+1
+                            break
+                    elif token.pos_ == "VERB":
+                        lemma = (token.text, token.lemma_)
+                        lemmas.append(lemma)
+                if index != -1:
+                    for j in range(index, len(span)):
+                        token = span[j]
+                        if token.pos_ == "NOUN" and span[j+1].pos_ != "NOUN":
+                            purpose2 = span[:j+1]
+                            purpose.append(purpose2.text)
 
-            if index != -1:
-                for j in range(index, len(span)):
-                    token = span[j]
-                    if token.pos_ == "NOUN" and span[j+1].pos_ != "NOUN":
-                        purpose2 = span[:j+1]
-                        purpose.append(purpose2.text)
-
-            sz = len(purpose)
-            for i in range(sz):
-                item = purpose[i]
-                new_item = 'medicine to ' + item
-                purpose.append(new_item)
-            return purpose
+                sz = len(purpose)
+                for i in range(sz):
+                    item = purpose[i]
+                    new_item = 'medicine to ' + item
+                    purpose.append(new_item)
+                return purpose
     return purpose
 
 
-# receives url of spcpil drug leaflet, returns lists of purposes for the drug if any can be found, otherwise returns empty list
-# Like http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1440737849470.pdf
-def get_purpose(paras, nlp):
-    heading_matches = ['What[\w\s]+are and what they are used for.', 'What[\w\s]+is and what it is used for']
+# receives spcpil url of drug leaflet and returns aliases
+# what order do we want for most efficient search results?
+# https://spacy.io/usage/linguistic-features
+# call function for all relevant leaflets under drug name
+def get_aliases(url):
+    nlp = spacy.load('en')  # load vocab
+    text = GParser.convert_pdf(url, format='text')
+    xmldoc = GParser.convert_pdf(url, format='xml')
+    paras = ExtractParasLflt.extract_paragraphs(xmldoc)
+    aliases = []
+    common_title = re.search('Before you take[\w\s]+\n', text)
+    if common_title != None:
+        common_title = common_title.group(0)
+        common_title = re.sub('Before you take ', '', common_title)
+        if(common_title.endswith('Tablets')):
+            aliases.append(common_title.replace('Tablets', '').strip()) # check this works
+        aliases.append(common_title)
+
+    actv_subs = get_active_subst(text, nlp)
+
+    if actv_subs != []:
+        for active_sub in actv_subs:
+            aliases.append(active_sub)
+
+    # get purpose and get group of medicines it belongs to
+    heading_matches = [('What[\w\s]+are and what they are used for', 'What', 'are and what they are used for' ), ('What[\w\s]+is and what it is used for', 'What', 'is and what it is used for' )]
     groups_matches = [('[\w\s] belongs to a group of medicines called [\'|\‘|\"|\‘][\w\s|-]+[\'|\‘|\"|\'],*[\w\s]+', '[\w\s] belongs to a group of medicines called'), ('[\w\s] belongs to a group of medicines called[\w\s].', '[\w\s] belongs to a group of medicines called'), ('[\w\s]+ belongs to a group of medicines known as [\w\s]+.', '[\w\s]+ belongs to a group of medicines known as')]
     group = []
-    purpose = ''
+    purposes = []
     for para in paras:
         para = para.replace("\n", "")  # get rid of any new line symbols that mess up the search
         for heading in heading_matches: # get relevant paragraph
-            srch = re.search(heading, para)
+            srch = re.search(heading[0], para)
             if srch != None:
+                title = srch.group(0)
+                title = re.sub(heading[1], '', title)
+                title = re.sub(heading[2], '', title)
+                title = title.strip()
+                if title != '':
+                    aliases.append(title)
+                if not title.endswith('Tablets'):
+                    title = title + ' ' + 'Tablets'
+                    aliases.append(title)
                 for phrase_pair in groups_matches:
                     sentence = re.search(phrase_pair[0], para)
                     if sentence != None:
                         sentence =sentence.group(0)
                         sentence = re.sub(phrase_pair[1], '', sentence)
-
                         doc_sentence = nlp(u'' + sentence)
                         curr_group = ''
                         for tok in doc_sentence:
@@ -157,73 +187,14 @@ def get_purpose(paras, nlp):
                                 group.append(re.sub('-', ' ', curr_group))
                                 group.append(re.sub('-', '', curr_group))
                         sentence = re.sub(curr_group, '', sentence).strip()
+        if purposes == []:
+            purpose = get_med_for(para, nlp)
+            if purpose != []:
+                purposes += purpose
 
-                        purpose_srch = sentence
-                        purpose = get_med_for(para, nlp)
-                        return group, purpose
+    group_of_meds = group
 
-                # extract purpose e.g. 'medicine to help stop blood clots'
-                stop_synonyms = ['stop', 'prevent', 'inhibit']
-
-
-
-    return '', ''
-
-
-
-# receives spcpil url of drug leaflet and returns aliases
-# what order do we want for most efficient search results?
-# https://spacy.io/usage/linguistic-features
-# call function for all relevant leaflets under drug name
-def get_aliases(url, product):
-    nlp = spacy.load('en')  # load vocab
-    text = GParser.convert_pdf(url, format='text')
-    xmldoc = GParser.convert_pdf(url, format='xml')
-    paras = ExtractParasLflt.extract_paragraphs(xmldoc)
-    aliases = []
-    common_title = re.search('Before you take[\w\s]+\n', text)
-    if common_title != None:
-        common_title = common_title.group(0)
-        common_title = re.sub('Before you take ', '', common_title)
-        if(common_title.endswith('Tablets')):
-            aliases.append(common_title.replace('Tablets', '').strip()) # check this works
-        aliases.append(common_title)
-
-    for para in paras:
-        if para != '':
-            title = para
-            title = re.sub('[ ]{2,}', ' ', title)  # replace multiple spaces by one space
-            title = re.sub('\n', '', title)
-            title = re.sub('®', '', title).strip()
-            title = re.sub('<br>|<b>', '', title)
-            title = re.sub('</br>|</b>', '', title)
-            title = re.sub('\([\w]+\)', '', title)
-            title = title.strip()
-            aliases.append(title)
-            if title.endswith('Tablets'):
-                title.replace('Tablets', '')
-                title.strip()
-                dosage_srch = re.search('\d', title)
-                if dosage_srch != None:
-                    title = re.sub('<br>|<b>', '', title)
-                    title = re.sub('</br>|</b>', '', title)
-                    title = title[:dosage_srch.start()].strip()
-
-                    aliases.append(title)
-            break
-
-    actv_subs = get_active_subst(text, nlp)
-    if actv_subs != []:
-        for active_sub in actv_subs:
-            aliases.append(active_sub)
-
-    # get purpose and get group of medicines it belongs to
-    group_of_meds, purpose = get_purpose(paras, nlp)
-    if group_of_meds != []:
-        aliases += group_of_meds
-    if purpose != '':
-        aliases.append(purpose)
-    return aliases
+    return aliases, group_of_meds, purposes
 
 def conflicting_conditions(paras, nlp):
     # loop through paras and get bit with if... if ... if ... bullet points
@@ -242,23 +213,25 @@ def conflicting_conditions(paras, nlp):
 
 def test_aliases():
     # text = GParser.convert_pdf(url, format='text')
-    urls = ['http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1516338822280.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1440737849470.pdf']
+    urls = ['http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1516338822280.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1440737849470.pdf', 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1487918987625.pdf']
     correct_group = [['antithrombotics', 'antithrombotic'], ['anti-thrombotic agents', 'anti-thrombotic agent', 'anti thrombotic agents', 'antithrombotic agents']]
     correct_purpose = ['prevent blood clots', 'stop blood clots forming']
     nlp = spacy.load('en')  # load vocab
     for i in range(len(urls)):
+        if i != 2:
+            continue
         xmldoc = GParser.convert_pdf(urls[i], format='xml')
         paras = ExtractParasLflt.extract_paragraphs(xmldoc)
         text = GParser.convert_pdf(urls[i], format='text')
-        group_of_meds, purpose = get_purpose(paras, nlp)
+        # group_of_meds, purpose = get_purpose(paras, nlp)
         # print(group_of_meds)
         # print(purpose)
         # print (group_of_meds == correct_group[i])
         # print (purpose == correct_purpose[i])
-        print(get_aliases(urls[i], ''))
+        print(get_aliases(urls[i]))
         # print(get_active_subst(text, nlp))
 
-# test_aliases()
+test_aliases()
 
 
 
@@ -270,8 +243,9 @@ def test_extract_purpose():
         purpose = get_med_for(t, nlp)
         print(purpose)
 
-test_extract_purpose()
+# test_extract_purpose()
 # url = 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1440737849470.pdf'
 # url = 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1515129004813.pdf'
 # url = 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1450423174307.pdf'
 # url = 'http://www.mhra.gov.uk/home/groups/spcpil/documents/spcpil/con1516338822280.pdf'
+# print(get_med_for('Topical corticosteroids are able to reduce the inflammation caused by a variety of skin conditions, and so allow it to get better.', spacy.load('en')))
